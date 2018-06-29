2ef3ab73c892ce707be97553285b4cb5584c8761
Rikuo Hasegawa <rikuo.hase1997@gmail.com>
2018-06-29 20:37:28 +0900

    progress

--- src/js/client.js
+++ src/js/client.js
@@ -1,80 +1,115 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack } from 'victory';
+import {VictoryLegend, VictoryLine, VictoryChart, VictoryTheme, VictoryArea, VictoryGroup} from 'victory'
+import data from './data'
 
-const data2012 = [
-  {quarter: 1, earnings: 13000},
-  {quarter: 2, earnings: 16500},
-  {quarter: 3, earnings: 14250},
-  {quarter: 4, earnings: 19000}
-];
+const toXY = (arr) => {
+  let reg = arr.length - 1
+  return arr.map((y, i) => ({x: i/reg, y:y}))
+}
 
-const data2013 = [
-  {quarter: 1, earnings: 15000},
-  {quarter: 2, earnings: 12500},
-  {quarter: 3, earnings: 19500},
-  {quarter: 4, earnings: 13000}
-];
-
-const data2014 = [
-  {quarter: 1, earnings: 11500},
-  {quarter: 2, earnings: 13250},
-  {quarter: 3, earnings: 20000},
-  {quarter: 4, earnings: 15500}
-];
-
-const data2015 = [
-  {quarter: 1, earnings: 18000},
-  {quarter: 2, earnings: 13250},
-  {quarter: 3, earnings: 15000},
-  {quarter: 4, earnings: 12000}
-];
+const zip = (xs,ys) => {
+  return xs.map((x,i) => ({x: x, y: ys[i]}))
+}
 
 class Main extends React.Component {
   render() {
+    const box = this.props.box
+
+    let externalEventMutations = undefined
+
+    const removeMutation = () => {
+      externalEventMutations = undefined
+    }
+
+    const clearAndHighlight = (props) => {
+      externalEventMutations = [
+        {
+          childName: "legend",
+          target: ["data"],
+          eventKey: "all",
+          mutation: Object.assign(props, {style:
+            (() => {let s = props.style; s.opacity = 1; return s})()
+          }),
+          callback: removeMutation
+        }
+      ]
+      return Object.assign(props, {style:
+        (() => {let s = props.style; s.opacity = 0.1; return s})()
+      })
+    }
+
+    const eventNames = box.concat([{name: 'legend'}])
+
+    const eventHandler = eventNames.map(x => ({
+      target: "data"
+      ,childName: x.name
+      ,eventHandlers: {
+        onMouseOver: () => ({
+          target: "data"
+          ,mutation: clearAndHighlight
+        })
+      }
+    }))
+
+    const theme = VictoryTheme.material
+    theme.axis.style.axis.strokeWidth = 0.1
     return (
-      <div>
-        <h1>Victory Tutorial</h1>
+      <div className="graph">
         <VictoryChart
-          domainPadding={10}
-          theme={VictoryTheme.material}
+        theme={theme}
+        domain={{x: [-0.1, 1.1], y: [-0.1, 1.1]}}
+        events={eventHandler}
+        externalEventMutations={externalEventMutations}
         >
-          <VictoryAxis
-            tickValues={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+          <VictoryLegend x={125} y={10}
+            name="legend"
+            orientation="horizontal"
+            gutter={20}
+            style={{ border: { stroke: "black" } }}
+            colorScale={box.map(x => x.color)}
+            data={box.map(x => ({name: x.name}))}
           />
-          <VictoryAxis
-            dependentAxis
-            tickFormat={(x) => (`$${x / 1000}k`)}
-          />
-          <VictoryStack
-            colorScale={"warm"}
-          >
-            <VictoryBar
-              data={data2012}
-              x={"quarter"}
-              y={"earnings"}
+          {box.map(d => (
+            <VictoryLine
+              name={d.name}
+              style={{data: {
+                stroke: d.color
+                ,strokeWidth: 3 }
+              }}
+              data={d.data}
+              x="x"
+              y="y"
             />
-            <VictoryBar
-              data={data2013}
-              x={"quarter"}
-              y={"earnings"}
-            />
-            <VictoryBar
-              data={data2014}
-              x={"quarter"}
-              y={"earnings"}
-            />
-            <VictoryBar
-              data={data2015}
-              x={"quarter"}
-              y={"earnings"}
-            />
-          </VictoryStack>
+          ))}
         </VictoryChart>
       </div>
-    );
+    )
   }
 }
 
+const keys = [0,1,2, "micro", "macro"]
+const datas = []
+for (let i of keys){
+  let d = zip(data.fpr[`${i}`], data.tpr[i])
+  datas.push(d)
+}
+
+const strokes = [
+  "#628395"
+  ,"#04151F"
+  ,"#C44900"
+  ,"#575C55"
+  ,"#ACC12F"
+]
+
+const box = [...Array(5).keys()].map((_, i) => ({
+  data: datas[i]
+  ,color: strokes[i]
+  ,name: `label${keys[i]}`
+}))
+
+
+
 const app = document.getElementById('app');
-ReactDOM.render(<Main />, app);
+ReactDOM.render(<Main box={box} />, app);
